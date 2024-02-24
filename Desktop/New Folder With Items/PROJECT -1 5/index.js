92eef24a3529619de84a6d0debe8d367b9b1e393
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2024-02-24 20:06:41 +0530

    facebook login added

--- Desktop/New Folder With Items/PROJECT -1 5/index.js
+++ Desktop/New Folder With Items/PROJECT -1 5/index.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/BESPOKE");
+const User = require("./model/userModel")
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -7,7 +10,7 @@ require("dotenv").config();
 require("./auth");
 const passport = require("passport");
 
-// ____________ Session Middleware ______________
+// Session Middleware
 app.use(
   session({
     secret: process.env.SECRET,
@@ -16,28 +19,44 @@ app.use(
   })
 );
 
-// ____________ Passport Middleware ______________
+// Passport Middleware
 app.use(passport.initialize());
-app.use(passport.session()); // <-- Adding passport.session() middleware
+app.use(passport.session());
 
-// ____________ Google Authentication Routes ______________
+// Google Authentication Routes
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['email', 'profile'] })
 );
 
 app.get('/auth/google/callback',
   passport.authenticate('google', {
-    successRedirect: '/auth/google/success',
-    failureRedirect: '/auth/google/failure'
+    successRedirect: '/auth/google',
+    failureRedirect: '/auth/google'
   })
 );
 
 app.get("/auth/protected", (req, res) => {
-  const name = req.user.displayName;
-  res.send(`hello ${name}`);
+  if (req.isAuthenticated()) {
+    const name = req.user.displayName;
+    res.send(`hello ${name}`);
+  } else {
+    res.redirect("/login"); // Redirect to login page if not authenticated
+  }
 });
 
-// _____________ Set view engine_______________
+// facebook Authentication Routes
+app.get('/auth/facebook',
+  passport.authenticate('facebook', {}));
+
+  app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/register' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+
+
+
 // Set view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -46,7 +65,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(nocache());
 
-// routes------------------
+// Routes
 const userRoute = require("./routes/userroute");
 app.use("/", userRoute);
 
@@ -55,6 +74,6 @@ app.use("/admin", adminRoute);
 
 const port = 3009;
 app.listen(port, () => {
-  console.log(`server running on http://localhost:${port}`);
-  console.log(`server running on http://localhost:${port}/admin`);
+  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}/admin/`);
 });
